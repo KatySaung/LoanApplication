579b0577f79c70a17c81f1e13de8ca54afe56483
Katherine <Katherine.Saung@peopleshores.com>
2024-10-08 17:23:10 -0400

    debugging controller test method to createCustomer with http 403 error

--- src/test/java/com/example/loanApplication/controllers/TestCustomerController.java
+++ src/test/java/com/example/loanApplication/controllers/TestCustomerController.java
@@ -3,6 +3,7 @@ package com.example.loanApplication.controllers;
 import com.example.loanApplication.dtos.CustomerDTO;
 import com.example.loanApplication.entities.Customer;
 import com.example.loanApplication.services.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,19 +11,15 @@ import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -44,8 +41,7 @@ public class TestCustomerController {
 
     private Long customerId;
     private CustomerDTO customerDTO;
-
-
+    private Customer customer;
 
     @BeforeEach
     public void setUp(){
@@ -59,83 +55,76 @@ public class TestCustomerController {
                 Collections.emptyList()
         );
 
-    }
-
-    //Helper method to convert customer to customerDTO to reduce boilerplate code in test create Customer method
-    private Customer createCustomerEntity(){
-        Customer customer = new Customer();
-        customer.setCustomerId(customerId);
+        customer = new Customer();
         customer.setName(customerDTO.name());
         customer.setEmail(customerDTO.email());
         customer.setAge(customerDTO.age());
         customer.setUsername(customerDTO.username());
         customer.setRoles(Collections.emptyList());
-        return customer;
 
     }
 
     //Success (Controller perform correct CRUD methods under normal conditions)
     @Test
     public void testCreateCustomer() throws Exception {
-//        Customer createdCustomer= createCustomerEntity();
 
         //Arrange: Behavior of mock service
-        when(customerService.createCustomer(any())).thenReturn(createCustomerEntity());
+//        Customer savedCustomer = new Customer();
+//        savedCustomer.setCustomerId(1L);
+//        savedCustomer.setName(customerDTO.name());
+//        savedCustomer.setEmail(customerDTO.email());
+//        savedCustomer.setAge(customerDTO.age());
+//        savedCustomer.setUsername(customerDTO.username());
+//        savedCustomer.setRoles(customerDTO.roles());
 
-//        //Act: Call controller method
-//        ResponseEntity<CustomerDTO> response = customerController.createCustomer(customerDTO);
-//
-//        //Assert: Check response status
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//
-//        //Assert: make sure the response body is not null
-//        CustomerDTO responseBody = response.getBody(); //make sure response body is not null
-//        assertNotNull(responseBody, "Response body cannot be null");
-//
-//        //Assert: Check response body
-//        assertEquals(customerDTO.name(), response.getBody().name());
+        when(customerService.createCustomer(any(CustomerDTO.class))).thenReturn(customer);
+
         mockMvc.perform(post("/customers")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString("bhumphrey")))
-                .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.age").value(100));
+                        .content(objectMapper.writeValueAsString(customerDTO)))
+                .andExpect(status().isCreated());
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(jsonPath("$.id").value(1L))
+//                .andExpect(jsonPath("$.name").value("Bobby Humphrey"))
+//                .andExpect(jsonPath("$.email").value("bobbyhumphrey@fakemail.com"))
+//                .andExpect(jsonPath("$.age").value(100));
 
         //verify()
+        verify(customerService, times(1)).createCustomer(any(CustomerDTO.class));
 
     }
 
     //Retrieve customer
-    @Test
-    public void testGetCustomerById(){
-        //Arrange:
-        when(customerService.getCustomerById(customerId)).thenReturn(customerDTO);
-
-        //Act
-        ResponseEntity<CustomerDTO> response = customerController.getCustomerById(customerId);
-
-        //Assert
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(customerDTO, response.getBody());
-
-    }
+//    @Test
+//    public void testGetCustomerById(){
+//        //Arrange:
+//        when(customerService.getCustomerById(customerId)).thenReturn(customerDTO);
+//
+//        //Act
+//        ResponseEntity<CustomerDTO> response = customerController.getCustomerById(customerId);
+//
+//        //Assert
+//        assertEquals(HttpStatus.OK, response.getStatusCode());
+//        assertEquals(customerDTO, response.getBody());
+//
+//    }
 
     //Update Customer
-    @Test
-    public void testUpdateCustomerId(){
-        //Arrange
-        customerId = 1L;
-        CustomerDTO updatedCustomerDTO = new CustomerDTO(
-                customerId,
-                //in test use assertThat to make assertions about outcomes of test or to use doThrow() to make an exception handling here?
-
-        );
+//    @Test
+//    public void testUpdateCustomerId(){
+//        //Arrange
+//        customerId = 1L;
+//        CustomerDTO updatedCustomerDTO = new CustomerDTO(
+//                customerId,
+//                //in test use assertThat to make assertions about outcomes of test or to use doThrow() to make an exception handling here?
+//
+//        );
         //Act
 
         //Assert
 
 
-    }
+//    }
 
 
     //Delete Customer
